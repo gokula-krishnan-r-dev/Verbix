27e1c14022567cfd6da48394c66d775a2a061504
gokula <gokulakrishnanr812@gmail.com>
2025-04-17 17:07:42 +0530

    Initial Commit

---
+++ Streamline/StreamlineApp.swift
@@ -0,0 +1,17 @@
+//
+//  StreamlineApp.swift
+//  Streamline
+//
+//  Created by gokul on 17/04/25.
+//
+
+import SwiftUI
+
+@main
+struct StreamlineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StreamlineTests/StreamlineTests.swift
@@ -0,0 +1,17 @@
+//
+//  StreamlineTests.swift
+//  StreamlineTests
+//
+//  Created by gokul on 17/04/25.
+//
+
+import Testing
+@testable import Streamline
+
+struct StreamlineTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
